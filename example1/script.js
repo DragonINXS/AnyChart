b119acc693a0fc871de239e0874a0e84e6dc0f1e
dragonINXS <davidmudd@outlook.com>
2018-11-21 10:57:48 -0500

    working out use case

--- example1/script.js
+++ example1/script.js
@@ -1,9 +1,10 @@
 anychart.onDocumentReady(function () {
+  
   var data = [
-      {x: 'A', value: 20},
+      {x: 'A', value: 20}, // ingredient/wine and value would be the count of it's pairings
       {x: 'B', value: 12},
       {x: 'C', value: 17},
-      {x: 'A&B', value: 4},
+      {x: 'A&B', value: 4}, // would be the shared pairings
       {x: 'B&C', value: 5},
       {x: 'A&C', value: 7},
       {x: 'A&B&C', value: 1}
